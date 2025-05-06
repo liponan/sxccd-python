8b8a17c3ce4a2d8d3b8e16b6dc1e6a31db3f5999
Po-Nan Li <663444+liponan@users.noreply.github.com>
2025-05-05 19:33:03 -0700

    Update sxccd_utils.py

--- sxccd/sxccd_utils.py
+++ sxccd/sxccd_utils.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-def decLH( LH ):
+
+def decLH(LH):
     return LH[0] + 256*LH[1]
 
-def dec2bytes( num, lg=2 ):
+
+def dec2bytes(num, lg=2):
     return (num).to_bytes(lg, byteorder="little")
 
-def dec2image( data, h, w ):
+
+def dec2image(data, h, w):
     img_sz = int(len(data)/2)
-    img = np.zeros( (img_sz,1), dtype=np.uint32)
+    img = np.zeros((img_sz,1), dtype=np.uint32)
     for i in range(img_sz):
         img[i] = decLH( data[2*i:(2*i+2)])
-    img = np.reshape( img, (h, w) )
+    img = np.reshape(img, (h, w))
     return img
